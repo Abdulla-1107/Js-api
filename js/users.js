93b6dfd4a26aed902bc5982779f0cc9345c63c9d
Abdulla <abdusattorovabdulla60@gmail.com>
2025-05-01 07:51:13 +0500

    second api

---
+++ js/users.js
@@ -0,0 +1,62 @@
+const BASE_URL = "https://dummyjson.com/";
+
+function hideLoading() {
+  document.querySelector(".loading").style.display = "none";
+}
+
+function showSkeletons(count = 15) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  for (let i = 0; i < count; i++) {
+    const skeleton = document.createElement("div");
+    skeleton.className = "skeleton";
+    container.appendChild(skeleton);
+  }
+}
+
+function renderUsersData(data) {
+  const container = document.querySelector(".container");
+  const fragment = document.createDocumentFragment();
+  container.innerHTML = "";
+
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <img src="${item.image}" alt="${item.name}" />
+      <div class="card-body">
+      <p>Age: ${item.age}</p>
+        <h3>${item.firstName}</h3>
+        <h3>${item.lastName}</h3>
+        <p>ID: ${item.id}</p>
+        <a class="btn" href="#">View User</a>
+      </div>
+    `;
+    fragment.appendChild(card);
+  });
+
+  container.appendChild(fragment);
+}
+
+function fetchData(endpoint) {
+  showSkeletons(); 
+  fetch(`${BASE_URL}${endpoint}`)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      hideLoading(); 
+      renderUsersData(data.users); 
+    })
+    .catch((err) => {
+      hideLoading();
+      console.log(err);
+    });
+}
+
+window.addEventListener("load", () => {
+  fetchData("users");
+});
